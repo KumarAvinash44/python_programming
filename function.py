a393754d7b5c01ed9e2657011a64f661f70c5511
Avinash Kumar <kumar@soongsil.ac.kr>
2023-12-21 13:56:38 +0900

    functions working

--- function.py
+++ function.py
@@ -4,3 +4,20 @@ def to_celsius(x):
 
 temp = to_celsius(100)
 print(temp)
+
+# Function: A body of resuable code for performing specific processes or tasks
+
+def greeting(name):
+   print(f'Welcome, {name}!')
+   print(f'You are part of the team!')
+
+greeting("Avinash Kumar")
+
+# return: A reserved keyword in Python that makes a function produce new results, which are saved for later use
+
+def area_triangle(base, height):
+   return base * height / 2
+
+area = area_triangle(4,4)
+
+print(f'The area of the triangle is {area}')
