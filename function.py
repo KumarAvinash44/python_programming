6b9384365ecddcb5f6d5a57575c9eec50dca0970
Avinash Kumar <kumar@soongsil.ac.kr>
2023-12-21 14:12:14 +0900

    function working

--- function.py
+++ function.py
@@ -21,3 +21,36 @@ def area_triangle(base, height):
 area = area_triangle(4,4)
 
 print(f'The area of the triangle is {area}')
+
+# Resuability: Defining code once and using it many times without having to rewrite it.
+
+def get_seconds(hours, minutes, seconds):
+   total_seconds = hours * 3600 + minutes * 60 + seconds
+   return total_seconds
+
+
+total_seconds = get_seconds(1,1,60)
+print(f'Total number of seconds is {total_seconds}')
+
+def lucky_number(name):
+   number = len(name)*9
+   print(f"Hello {name}. Your lucky number is {number}")
+
+lucky_number("Avinash")
+lucky_number("Kumar")
+
+
+# Modularity: The ability to write code in separate components that work together and that can be reused for other programs
+# Refactoring: The process of restructuring code while maintaining its original functionality
+# Self-documenting code: Code written in a way that is readable and makes its purpose clear
+
+def factorial(number):
+    fact = 1
+    for i in range(number):
+        fact = fact * (i+1)
+
+    return fact
+
+number = input("Enter any number: ")
+number = int(number)
+print(f"The factorial of {number} is {factorial(number)}")
