8e04529c9d38d11cc366ed58213b063aa8ff0ea8
Avinash Kumar <kumar@soongsil.ac.kr>
2023-12-21 09:38:11 +0900

    working

--- hello.py
+++ hello.py
@@ -2,3 +2,15 @@ print("Hello Word!")
 print(22)
 
 print((5 + 4)/4) 
+
+country = "Brazil"
+age = 30
+
+print(country)
+print(age)
+
+print(10**3 == 1000)
+
+print (10 * 3 == 40)
+
+print(10 * 3 == age)
