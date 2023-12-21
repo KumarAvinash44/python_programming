2d68c3c84df705a84caa14ae419984ea81db6263
Avinash Kumar <kumar@soongsil.ac.kr>
2023-12-21 10:23:26 +0900

    variable working

--- variable.py
+++ variable.py
@@ -4,6 +4,11 @@ age_list = [34, 25, 23, 19, 29]
 max_age = max(age_list)
 print(f'maximum age value: {max_age}')
 
+min_age = min(age_list)
+
+print(f'the difference between max age and min age is {max_age - min_age}')
+
+
 # change the type of max_age variable from integer to string
 max_age = str(max_age)
 print(f'converted type of the variable: {type(max_age)}')
@@ -12,4 +17,6 @@ print(f'converted type of the variable: {type(max_age)}')
 # reassign the total new value to the variable, reassign
 max_age = "ninety-nine"
 print(max_age)
+
+
 
